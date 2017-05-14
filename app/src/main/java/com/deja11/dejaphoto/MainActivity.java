ff2751bffa7afa243d50925c43478b93992e723b
Tee <ssrey@ucsd.edu>
2017-05-14 14:09:45 -0700

    DEJA11-65 ask permission to access storage at the very beginning

--- app/src/main/java/com/deja11/dejaphoto/MainActivity.java
+++ app/src/main/java/com/deja11/dejaphoto/MainActivity.java
@@ -10,6 +10,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
@@ -57,6 +58,10 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
+        }
+
 
         // initialize the value of the interval using shared preferences, if applicable
         SharedPreferences mSharedPref = PreferenceManager.getDefaultSharedPreferences(this);
