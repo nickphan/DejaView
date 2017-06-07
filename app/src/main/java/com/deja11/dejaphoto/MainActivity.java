aa74e6e4806c861b78a8ffa272fa11163c8f4a44
Carl <clt020@ucsd.edu>
2017-06-06 19:32:19 -0700

    DEJA11-93 Added intent for camera

--- app/src/main/java/com/deja11/dejaphoto/MainActivity.java
+++ app/src/main/java/com/deja11/dejaphoto/MainActivity.java
@@ -42,7 +42,9 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.channels.FileChannel;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -263,6 +265,14 @@ public class MainActivity extends Activity implements SharedPreferences.OnShared
     public void settingsClicked(View view) {
         Intent intent = new Intent(MainActivity.this, SettingPreference.class);
         startActivity(intent);
+    }
+
+    public void launchCamera(View view) {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if (cameraIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(cameraIntent);
+        }
     }
 
     /**
