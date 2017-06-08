cc3c39f1583735ca708664bfce518addba57121b
nickphan <ngphan@ucsd.edu>
2017-06-08 00:24:50 -0700

    Deja 11-71. Testing

--- app/src/main/java/com/deja11/dejaphoto/MainActivity.java
+++ app/src/main/java/com/deja11/dejaphoto/MainActivity.java
@@ -121,6 +121,7 @@ public class MainActivity extends Activity {
                     Log.e("the input username: ", email);
                     Log.d("To SharedPreference: ", email);
                     mSharedPrefcheck.edit().putString("username",email).apply();
+                    controller.createUser();
                 }
             });
             //pop out the window
